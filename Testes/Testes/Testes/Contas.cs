663a51aab3e3468c9d6c11f670d061417eda45e4
Danilo Gomes <dno.gomesps@gmail.com>
2022-01-31 18:53:23 -0300

    inclusão da aula de testes unitários

---
+++ Testes/Testes/Testes/Contas.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Testes
+{
+    public class Contas
+    {
+        public int Soma(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtracao(int a, int b)
+        {
+            return a - b;
+        }
+
+        public double Divisao(double a, double b)
+        {
+            return a / b;
+        }
+
+        public double Mutiplicacao(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
---
+++ Testes/Testes/TestesTests/ContasTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Testes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Testes.Tests
+{
+    [TestClass()]
+    public class ContasTests
+    {
+        [TestMethod()]
+        public void SomaTest()
+        {
+            int a = 5;
+            int b = 5;
+            int esperado = 10;
+
+            var contas = new Contas();
+            var result = contas.Soma(a, b);
+
+            Assert.AreEqual(esperado, result);
+        }
+
+        [TestMethod()]
+        public void SubtracaoTest()
+        {
+            int a = 10;
+            int b = 2;
+            int esperado = 8;
+
+            var contas = new Contas();
+            var result = contas.Subtracao(a, b);
+
+            Assert.AreEqual(esperado, result);
+        }
+
+        [TestMethod()]
+        public void DivisaoTest()
+        {
+            int a = 10;
+            int b = 2;
+            int esperado = 5;
+
+            var contas = new Contas();
+            var result = contas.Divisao(a, b);
+
+            Assert.AreEqual(esperado, result);
+        }
+
+        [TestMethod()]
+        public void MutiplicacaoTest()
+        {
+            int a = 10;
+            int b = 2;
+            int esperado = 20;
+
+            var contas = new Contas();
+            var result = contas.Mutiplicacao(a, b);
+
+            Assert.AreEqual(esperado, result);
+        }
+    }
+}
