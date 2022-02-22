0bb8e8a934093ff2607f1fee5d9b87b702bdc756
Danilo Gomes <dno.gomesps@gmail.com>
2022-02-21 21:09:29 -0300

    21-02-2022

---
+++ Primeiros passos com .NET/DigitalInnovationOne/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+
+
+namespace DigitalInnovationOne{
+    class Program{
+        static void Main(string[] args){
+            int numeroDeVez = 5;
+
+            for (int i=0; i<numeroDeVez; i++){
+                Console.WriteLine($"Bem-vindo ao curso de .Net {i}");
+            }
+        }
+    }
+}
